6a14e52e6f0070f4cd8f8b3e4a459dd078cbe2b5
MarlusCSilva <marlussilva5816@gmail.com>
2024-09-08 19:45:19 -0300

    Arrumar Eventos

--- resources/views/eventos/index.blade.php
+++ resources/views/eventos/index.blade.php
@@ -1,13 +1,31 @@
 @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 @include('partials.header')
-<h1 class="mt-5 p-5">Lista de Eventos</h1>
 
-@if($eventos->isEmpty())
-    <p>Não há eventos cadastrados.</p>
-@else
-    <ul>
-        @foreach($eventos as $evento)
-            <li>{{ $evento->nome }} - {{ $evento->data }}</li>
-        @endforeach
-    </ul>
-@endif
+<div class="container mt-5">
+    <h1 class="mb-5 text-center">Lista de Eventos</h1>
+
+    @if($eventos->isEmpty())
+        <div class="alert alert-warning text-center">
+            Não há eventos cadastrados.
+        </div>
+    @else
+        <div class="row">
+            @foreach($eventos as $evento)
+                <div class="col-md-4 mb-4">
+                    <div class="card" style="width: 18rem;">
+                    <img src="{{$evento->url}}" class="card-img-top" alt="...">  
+                        <div class="card-body">
+                            <h5 class="card-title">{{ $evento->nome }}</h5>
+                            <p class="card-text">Data: {{ $evento->data }}</p>
+                            <p class="card-text">Local: {{ $evento->localizacao}}</p>
+                            <p class="card-text">Hora: {{ $evento->hora}}</p>
+                            <p class="card-text">Descrição: {{ $evento->descricao}}</p>
+                            <p class="card-text">Numeros de Participantes: {{ $evento->maximo_participantes}}</p>
+                            <a href="#" class="btn btn-primary">Inscrever-se</a>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    @endif
+</div>
--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,4 +1,12 @@
-<footer>
-    <h1>EventFlow</h1>
-    <P>O melhor site de eventos do planeta!</P>
+<footer class="bg-dark text-white py-4">
+    <div class="container text-center">
+        <h1 class="mb-3">EventFlow</h1>
+        <p>O melhor site de eventos do planeta!</p>
+        <div class="d-flex justify-content-center">
+            <a href="#" class="text-white mx-2">Sobre nós</a>
+            <a href="#" class="text-white mx-2">Contato</a>
+            <a href="#" class="text-white mx-2">Política de Privacidade</a>
+        </div>
+        <p class="mt-3 mb-0">&copy; 2024 EventFlow. Todos os direitos reservados.</p>
+    </div>
 </footer>
