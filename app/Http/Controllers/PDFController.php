0fb191de4cd0d0892f7907872bdc1b81fb8a8eaa
MarlusCSilva <marlussilva5816@gmail.com>
2024-09-08 23:06:06 -0300

    Corrigido

--- app/Http/Controllers/PDFController.php
+++ app/Http/Controllers/PDFController.php
@@ -15,29 +15,32 @@ class PDFController extends Controller
         $pdf = new FPDF;
         $pdf->AddPage();
         $pdf->SetFont('Arial', 'B', 16);
-        $pdf->Image('img\logo-eventflow.png', 15, 3, 30); // Caminho, X, Y, largura
-
-        // Adicionar cabeçalho
         $pdf->Cell(0, 10, 'Relatorio dos Eventos', 0, 1, 'C');
-        $pdf->Ln(10);
+
+  
+        $pageWidth = $pdf->GetPageWidth();
+        $imageWidth = 30;
+        $x = ($pageWidth - $imageWidth) / 2;
+        $pdf->Image('img\logo-eventflow.png', $x, 15, $imageWidth);
+        $pdf->Ln(25);
         $fill = false;
 
-        $pdf->SetFillColor(256, 256, 256); // Cor de fundo para cabeçalhos
+        $pdf->SetFillColor(256, 256, 256);
         $pdf->SetFont('Arial', 'B', 12);
         $pdf->Cell(10, 10, 'ID', 1, 0, 'C', true);
         $pdf->Cell(43, 10, 'Nome', 1, 0, 'C', true);
         $pdf->Cell(30, 10, 'Data', 1, 0, 'C', true);
-        $pdf->Cell(30, 10, 'Descricao', 1, 0, 'C', true);
+        $pdf->Cell(30, 10, 'Status', 1, 0, 'C', true);
         $pdf->Cell(40, 10, 'Local', 1, 0, 'C', true);
         $pdf->Cell(40, 10, 'Hora', 1, 1, 'C', true);
 
-        // Adicionar os dados dos eventos
+ 
         $pdf->SetFont('Arial', '', 12);
 
-        // Define altura de linha e margem inferior
-    $alturaLinha = 10;
-    $margemInferior = 20;
-    $alturaPagina = $pdf->GetPageHeight();
+ 
+        $alturaLinha = 10;
+        $margemInferior = 20;
+        $alturaPagina = $pdf->GetPageHeight();
     
     // Adiciona os dados dos eventos
     foreach ($eventos as $evento) {
@@ -49,7 +52,7 @@ class PDFController extends Controller
             $pdf->MultiCell(10, 10, 'ID', 1, 0, 'C', true);
             $pdf->MultiCell(43, 10, 'Nome', 1, 0, 'C', true);
             $pdf->MultiCell(30, 10, 'Data', 1, 0, 'C', true);
-            $pdf->MultiCell(30, 10, 'Descrição', 1, 0, 'C', true);
+            $pdf->MultiCell(30, 10, 'Status', 1, 0, 'C', true);
             $pdf->MultiCell(40, 10, 'Local', 1, 0, 'C', true);
             $pdf->SetFont('Arial', '', 12);
         }
@@ -58,7 +61,7 @@ class PDFController extends Controller
         $pdf->Cell(10, $alturaLinha, $evento->id, 1,0, 'C', true);
         $pdf->Cell(43, $alturaLinha, $evento->nome, 1,0, 'C', true);
         $pdf->Cell(30, $alturaLinha, $evento->data, 1,0, 'C', true);
-        $pdf->Cell(30, $alturaLinha, $evento->descricao, 1,0, 'C', true);
+        $pdf->Cell(30, $alturaLinha, $evento->status, 1,0, 'C', true);
         $pdf->Cell(40, $alturaLinha, $evento->localizacao, 1,0, 'C', true);
         $pdf->Cell(40, $alturaLinha, $evento->hora, 1,0, 'C', true);
         $pdf->Ln();
