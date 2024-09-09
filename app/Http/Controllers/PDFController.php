2e94dde42a0e2044d6ba6c5af8ed85410550f36e
MarlusCSilva <marlussilva5816@gmail.com>
2024-09-08 22:23:33 -0300

    Finalizar Rota Gerar PDF

--- app/Http/Controllers/PDFController.php
+++ app/Http/Controllers/PDFController.php
@@ -12,7 +12,7 @@ class PDFController extends Controller
         $eventos = Evento::all();
 
         // Cria uma instância do FPDF
-        $pdf = new FPDF();
+        $pdf = new FPDF('L', 'mm', 'A4');
         $pdf->AddPage();
         $pdf->SetFont('Arial', 'B', 16);
 
@@ -32,16 +32,37 @@ class PDFController extends Controller
 
         // Adicionar os dados dos eventos
         $pdf->SetFont('Arial', '', 12);
-        foreach ($eventos as $evento) {
-            $pdf->Cell(10, 10, $evento->id, 1, 0, 'C', $fill);
-            $pdf->Cell(43, 10, $evento->nome, 1, 0, 'C', $fill);
-            $pdf->Cell(30, 10, $evento->data, 1, 0, 'C', $fill);
-            $pdf->Cell(30, 10, $evento->descricao, 1, 0, 'C', $fill);
-            $pdf->Cell(40, 10, $evento->localizacao, 1, 0, 'C', $fill);
-            $pdf->Cell(40, 10, $evento->hora, 1, 1, 'C', $fill);
-            $pdf->Ln();
-            
+
+        // Define altura de linha e margem inferior
+    $alturaLinha = 10;
+    $margemInferior = 20;
+    $alturaPagina = $pdf->GetPageHeight();
+    
+    // Adiciona os dados dos eventos
+    foreach ($eventos as $evento) {
+        if ($pdf->GetY() + $alturaLinha > $alturaPagina - $margemInferior) { // Verifica se há espaço suficiente para a linha na página atual
+            $pdf->AddPage(); // Adiciona uma nova página se necessário
+
+            // Re-adiciona cabeçalhos na nova página
+            $pdf->SetFillColor(200, 220, 255); // Cor de fundo para cabeçalhos
+            $pdf->SetFont('Arial', 'B', 12);
+            $pdf->MultiCell(10, 10, 'ID', 1, 0, 'C', true);
+            $pdf->MultiCell(43, 10, 'Nome', 1, 0, 'C', true);
+            $pdf->MultiCell(30, 10, 'Data', 1, 0, 'C', true);
+            $pdf->MultiCell(30, 10, 'Descrição', 1, 0, 'C', true);
+            $pdf->MultiCell(40, 10, 'Local', 1, 0, 'C', true);
+            $pdf->SetFont('Arial', '', 12);
         }
+
+        // Adiciona uma linha da tabela
+        $pdf->Cell(10, $alturaLinha, $evento->id, 1,0, 'C', true);
+        $pdf->Cell(43, $alturaLinha, $evento->nome, 1,0, 'C', true);
+        $pdf->Cell(30, $alturaLinha, $evento->data, 1,0, 'C', true);
+        $pdf->Cell(30, $alturaLinha, $evento->descricao, 1,0, 'C', true);
+        $pdf->Cell(40, $alturaLinha, $evento->localizacao, 1,0, 'C', true);
+        $pdf->Cell(40, $alturaLinha, $evento->hora, 1,0, 'C', true);
+        $pdf->Ln();
+    }
          // Adicionar rodapé
         $pdf->SetY(265);
         $pdf->SetFont('Arial', 'I', 10);
