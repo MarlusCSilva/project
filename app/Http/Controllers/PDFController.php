6d116fa3f6fb7ce3f931add05a5d61b15938e9ee
MarlusCSilva <marlussilva5816@gmail.com>
2024-09-08 21:13:00 -0300

    Gerado de PDF Arrumando

--- app/Http/Controllers/PDFController.php
+++ app/Http/Controllers/PDFController.php
@@ -16,16 +16,31 @@ class PDFController extends Controller
         $pdf->AddPage();
         $pdf->SetFont('Arial', 'B', 16);
 
-        // Adiciona cabeçalhos das colunas
-        $pdf->Cell(60, 10, 'Nome do Evento', 1);
-        $pdf->Cell(60, 10, 'Data do Evento', 1);
-        $pdf->Ln();
-        // Adiciona os dados dos eventos
+        // Adicionar cabeçalho
+        $pdf->Cell(0, 10, 'Relatorio dos Eventos', 0, 1, 'C');
+        $pdf->Ln(10);
+        $fill = false;
+
+        $pdf->SetFillColor(200, 220, 255); // Cor de fundo para cabeçalhos
+        $pdf->SetFont('Arial', 'B', 12);
+        $pdf->Cell(50, 10, 'Nome', 1, 0, 'C', true);
+        $pdf->Cell(30, 10, 'Data', 1, 0, 'C', true);
+        $pdf->Cell(30, 10, 'Descricao', 1, 0, 'C', true);
+        $pdf->Cell(40, 10, 'Local', 1, 1, 'C', true);
+
+        // Adicionar os dados dos eventos
+        $pdf->SetFont('Arial', '', 12);
         foreach ($eventos as $evento) {
-            $pdf->Cell(60, 10, $evento->nome, 1);
-            $pdf->Cell(60, 10, $evento->data, 1);
+            $pdf->Cell(50, 10, $evento->nome, 1, 0, 'L', $fill);
+            $pdf->Cell(30, 10, $evento->data, 1, 0, 'C', $fill);
+            $pdf->Cell(30, 10, $evento->descricao, 1, 0, 'L', $fill);
+            $pdf->Cell(40, 10, $evento->localizacao, 1, 1, 'L', $fill);
             $pdf->Ln();
         }
+         // Adicionar rodapé
+        $pdf->SetY(-15);
+        $pdf->SetFont('Arial', 'I', 10);
+        $pdf->Cell(0, 10, 'Gerado por EventFlow', 0, 0, 'C');
 
         // Gera o PDF na saída
         return response($pdf->Output(), 200)->header('Content-Type', 'application/pdf');
